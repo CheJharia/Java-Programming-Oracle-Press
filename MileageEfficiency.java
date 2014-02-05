eee17eccb2950a3a2ccdb9059785660b8470c19e
che.joharia <chejoharia@eisti.fr>
2014-02-05 22:58:05 +0100

    Demonstrate an interface that extends another interface
    Extended interface definition is in MileageEfficiency, and
    EnhancedTestDrive is a test

--- MileageEfficiency.java
+++ MileageEfficiency.java
@@ -2,6 +2,13 @@ interface MileageEfficiency {
 	public float getMilesPerGallon();
 }
 
+// an interface that extends the MileageEfficiency interface
+interface ExtendedMileageEfficiency extends MileageEfficiency {
+	public float getFuelEfficiency();
+
+	public float getElectricEfficiency();
+}
+
 class GasVehicle implements MileageEfficiency {
 
 	private float fuelConsumed;
@@ -27,10 +34,39 @@ class ElectricVehicle implements MileageEfficiency {
 	public float getMilesPerGallon() {
 		return tripCounter / kwPowerConsumed;
 	}
-	
+
 	public void makeTrip() {
 		tripCounter = 100;
 		kwPowerConsumed = 5.6f;
 	}
 
 }
+
+class HybridVehicle implements ExtendedMileageEfficiency {
+	private float tripCounter;
+	private float fuelConsumed;
+	private float kwPowerConsumed;
+
+	@Override
+	public float getMilesPerGallon() {
+		return 0.8f * getFuelEfficiency() + 1.12f % getElectricEfficiency();
+	}
+
+	@Override
+	public float getFuelEfficiency() {
+		return tripCounter / fuelConsumed;
+	}
+
+	@Override
+	public float getElectricEfficiency() {
+		return tripCounter / kwPowerConsumed;
+	}
+	
+	public void makeTrip(){
+		tripCounter = 100;
+		fuelConsumed = 4.1f;
+		kwPowerConsumed = 3.4f;
+	}
+
+}
+
