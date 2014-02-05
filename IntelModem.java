6ed4b6e2b92cae3d76245988182740b2f226e3de
che.joharia <chejoharia@eisti.fr>
2014-02-05 22:29:28 +0100

    Add IntelModem.java
    It implements Modem

---
+++ IntelModem.java
@@ -0,0 +1,28 @@
+
+public class IntelModem implements Modem{
+
+	@Override
+	public boolean open() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean close() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int read() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int write(byte[] buffer) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
