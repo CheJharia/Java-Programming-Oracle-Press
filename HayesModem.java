f7a684cd3aa581b350e251e8999c0ccfe254e93f
che.joharia <chejoharia@eisti.fr>
2014-02-08 18:50:08 +0100

    Delete @Override

--- HayesModem.java
+++ HayesModem.java
@@ -1,24 +1,20 @@
 public class HayesModem implements Modem {
 
-	@Override
 	public boolean open() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	public boolean close() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	public int read() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
 	public int write(byte[] buffer) {
 		// TODO Auto-generated method stub
 		return 0;
