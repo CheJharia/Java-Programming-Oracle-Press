0c2624390c1695ee475efe3cd2bb93984c2825e9
che.joharia <chejoharia@eisti.fr>
2014-02-08 19:12:29 +0100

    Double Nested Class

--- Outer.java
+++ Outer.java
@@ -1,12 +1,25 @@
 class Outer {
 	private int size = 10;
+	private int a = 20;
 
-	public class Inner {
+	class Inner {
 		private int size = 20;
-		public void someMethod(int size) {
+
+		public void someMethod(int size, final int b) {
 			System.out.println("Method parameter (size): " + size);
 			System.out.println("Inner size: " + this.size);
 			System.out.println("Outer size: " + Outer.this.size);
+
+			class InnerInner {
+				int c = 30;
+
+				public void innerMethod() {
+					System.out.println("Formal parameter (B): " + b);
+					System.out.println("Outer Class variable (A): " + a);
+					System.out.println("InnerInner Class variable (C): " + c);
+				}
+			}
+			new InnerInner().innerMethod();
 		}
 	}
 }
