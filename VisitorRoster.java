b9ee7ee5585a07fa94865ba4b64aeca717c5da09
che.joharia <chejoharia@eisti.fr>
2014-02-08 20:13:50 +0100

    OutofBoundsException not handled
    Not handling the OutOfBoundsException

---
+++ VisitorRoster.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+import org.omg.CORBA.PRIVATE_MEMBER;
+
+public class VisitorRoster {
+	private final int MAX_CAPACITY = 100;
+	private String[] visitors;
+
+	public static void main(String[] args) {
+		VisitorRoster roster = new VisitorRoster();
+		roster.init();
+		roster.registerVisitor();
+		roster.printVisitorList();
+	}
+
+	private void init() {
+		visitors = new String[MAX_CAPACITY];
+	}
+	
+	private void registerVisitor() {
+		Random r = new Random();
+		System.out.println("Registering visitors ...");
+		for (int i = 0; i < MAX_CAPACITY; i++) {
+			visitors[i] = Long.toString(Math.abs(r.nextLong()), 36);
+		}
+	}
+
+	private void printVisitorList() {
+		System.out.println("\nToday's Visitors:");
+		int i = 0;
+		while (i <= MAX_CAPACITY) {
+			System.out.println("Vistors ID # " + visitors[i++]);
+		}
+	}
+
+
+
+}
