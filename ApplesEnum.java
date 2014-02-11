3dd61c5c6c1338af089c7eaf037cd8ec842c250c
che.joharia <chejoharia@eisti.fr>
2014-02-11 01:02:53 +0100

    Add ApplesEnum.java
    *Enum definition with parameterised constructor.

---
+++ ApplesEnum.java
@@ -0,0 +1,28 @@
+enum Apple {
+	AURORA(10), BELMAC(12), CORTLAND(15), EMPIRE(8), GRAVENSTEIN(11);
+	private int price;
+
+	Apple(int price) {
+		this.price = price;
+	}
+
+	int getPrice() {
+		return price;
+	}
+	public String toString() {
+		String s = super.toString();
+		return s.substring(0, 1) + s.substring(1).toLowerCase();
+	}
+}
+
+public class ApplesEnum {
+
+	public static void main(String[] args) {
+		System.out.println("Apple price list:");
+		for (Apple apple : Apple.values()) {
+			System.out
+					.println(apple + " costs " + apple.getPrice() + " cents.");
+		}
+	}
+
+}
