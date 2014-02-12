a69569e36ae3b9aeb39249cb431e4ec77ef8fba6
che.joharia <chejoharia@eisti.fr>
2014-02-12 20:23:13 +0100

    Update ApplesEnum.java
    *Add a tab to print

--- ApplesEnum.java
+++ ApplesEnum.java
@@ -9,6 +9,7 @@ enum Apple {
 	int getPrice() {
 		return price;
 	}
+
 	public String toString() {
 		String s = super.toString();
 		return s.substring(0, 1) + s.substring(1).toLowerCase();
@@ -20,9 +21,16 @@ public class ApplesEnum {
 	public static void main(String[] args) {
 		System.out.println("Apple price list:");
 		for (Apple apple : Apple.values()) {
-			System.out
-					.println(apple + " costs " + apple.getPrice() + " cents.");
+			System.out.println("\t" + apple + " costs " + apple.getPrice()
+					+ " cents.");
 		}
+
+		System.out.println("Apple ordinal values(its possiton in the list of constants):");
+		for (Apple apple : Apple.values()) {
+			System.out
+					.println("\t" + "[" + apple + "]" + ":" + apple.ordinal());
+		}
+
 	}
 
 }
