adffa1633fb65269acbfbfa264059ada8362733a
che.joharia <chejoharia@eisti.fr>
2014-02-12 23:02:06 +0100

    Update CustomAnnotation.java
    *Add Retention(RetentionPolicy.RUNTIME)

--- CustomAnnotation.java
+++ CustomAnnotation.java
@@ -1,5 +1,11 @@
-public class CustomAnnotation {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+public class CustomAnnotation {
+	private int taxID;
+	
 	@WorkInProgress
 	@Task(description = "Implement tax computations", additionalNote = "This implementation is critical for the final launch", estimatedHours = 50, targetDate = "Jan 1, 2012")
 	public static float ComputeTax(float amount, float rate) {
@@ -7,11 +13,12 @@ public class CustomAnnotation {
 	}
 
 }
-
+@Retention(RetentionPolicy.RUNTIME)
 @interface WorkInProgress {
 
 }
-
+@Retention(RetentionPolicy.RUNTIME)
+@Target (ElementType.METHOD)
 @interface Task {
 
 	String description();
